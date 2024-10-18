5436a2106b592bd824eddc3f576c42b617ad4bde
NelsonCordoba <nelsoncordero1011@gmail.com>
2024-10-18 11:42:46 -0600

    No se lo que hize

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,3 +5,5 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True, null=True)
     birthday = models.DateField(blank=True, null=True)
+    def __str__(self):
+        return self.user.username
